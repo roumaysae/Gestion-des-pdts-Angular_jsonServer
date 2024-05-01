cb78ddb9ef7b8a8c53eb57207ffcd4b6ff54aa0e
Roumaysae <roumaysaeelamrani2002@gmail.com>
2024-05-01 12:33:02 +0100

    a good practice creation

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,33 +10,33 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {
   }
-
+public  host : string = "http://localhost:8080/";
   //1st method
   public searchProducts(keyword: string="",page:number=1 ,size:number=4 ){
-    return this.httpClient.get(`http://localhost:8080/products?name_like=${keyword}&_page=${page}&_limit=${size}`,{observe:"response"});
+    return this.httpClient.get(`${this.host}products?name_like=${keyword}&_page=${page}&_limit=${size}`,{observe:"response"});
   }
 
   //2nd method
   public checkProduct(product: Product): Observable<any> {
-    return this.httpClient.patch<Product>(`http://localhost:8080/products/${product.id}`,
+    return this.httpClient.patch<Product>(`${this.host}products/${product.id}`,
       {checked: !product.checked});
   }
 
   public deleteProduct(product: Product) {
-    return this.httpClient.delete<any>(`http://localhost:8080/products/${product.id}`);
+    return this.httpClient.delete<any>(`${this.host}products/${product.id}`);
   }
 
 //on va pas retounrer un prdt donc c'est void
 
  public addProduct(product: Product): Observable<Product> {
-     return this.httpClient.post<Product>(`http://localhost:8080/products`,product);
+     return this.httpClient.post<Product>(`${this.host}products`,product);
 }
 
   getProductById(productId: number): Observable<Product>{
-   return  this.httpClient.get<Product>(`http://localhost:8080/products/${productId}`);
+   return  this.httpClient.get<Product>(`${this.host}products/${productId}`);
   }
 
   updateProduct(product: Product): Observable<Product>{
-    return this.httpClient.put<Product>(`http://localhost:8080/products/${product.id}`,product);
+    return this.httpClient.put<Product>(`${this.host}products/${product.id}`,product);
   }
 }
